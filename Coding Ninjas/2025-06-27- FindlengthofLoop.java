ed30f00762f6c1a6452a9b372838d96513f96a67
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-06-27 12:34:45 +0530

    Create 2025-06-27- FindlengthofLoop.java

---
+++ Coding Ninjas/2025-06-27- FindlengthofLoop.java
@@ -0,0 +1,55 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static int lengthOfLoop(Node head) {
+        // Write your code here
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow==fast)
+            {   
+                fast = fast.next;
+                int count = 1;
+                while(fast!=slow)
+                {   
+                    fast = fast.next;
+                    count++;
+                }
+                return count;
+            }
+        }
+        return 0;
+    }
+}
