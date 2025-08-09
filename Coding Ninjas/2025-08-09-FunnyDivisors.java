6b1ea855da52e8d22095e892ecc3ed6a083ada1d
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-08-09 18:37:43 +0530

    Create 2025-08-09-FunnyDivisors.java

---
+++ Coding Ninjas/2025-08-09-FunnyDivisors.java
@@ -0,0 +1,17 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution {
+
+	public static int findSum(int n, ArrayList<Integer> arr) {
+		// Write your code here
+		int sum = 0;
+		for(int i = 0; i < arr.size(); i++){
+			if (arr.get(i) % 2 == 0 || arr.get(i) % 3 == 0){
+				sum += arr.get(i);
+			}
+		}
+		return sum;
+	}
+}
