e9d20beddd9c27f0756a7fa4a9961e667cbcfb53
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-08-17 13:11:32 +0530

    Create 2025-08-17-ContainerWithMostWater.java

---
+++ leetcode/2025-08-17-ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = Integer.MIN_VALUE;
+        int i = 0;
+        int j = height.length-1;
+        while(i<j)
+        {
+            maxArea = Math.max(maxArea,Math.abs((j-i))*(Math.min(height[i],height[j])));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+}
