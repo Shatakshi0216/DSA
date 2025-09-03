4bb156bf8b1f6066ea215876e33bf0e754985b95
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-09-03 14:43:20 +0530

    Create 2025-09-03-MinimumElementInBinarySearchTree.java

---
+++ Coding Ninjas/2025-09-03-MinimumElementInBinarySearchTree.java
@@ -0,0 +1,48 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node left;
+     public Node right;
+
+     Node()
+     {
+         this.data = 0;
+         this.left = null;
+         this.right = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.left = null;
+         this.right = null;
+     }
+
+     Node(int data, Node left, Node right)
+     {
+         this.data = data;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution
+{
+    public static int minValue(Node root) {
+        // Write your code here.
+        if(root==null) return -1;
+        Node temp = root;
+        while(temp.left!=null)
+        {
+            temp = temp.left;
+        }
+        return temp.data;
+
+    }
+}
