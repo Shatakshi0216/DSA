25c1514955dedc8b56a4fa402fae61ece4df532a
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-09-19 22:28:41 +0530

    Create 2025-09-19-.BestTimeToBuyAndSellStock.java

---
+++ leetcode/2025-09-19-.BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+/*
+Jha pr khride uske piche ka sbse min check kro 
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int mini = prices[0];
+        for(int i=1;i<prices.length;i++)
+        {
+            int cost = prices[i] - mini;
+            profit = Math.max(profit,cost);
+            mini = Math.min(prices[i],mini);
+        }
+        return profit;
+    }
+}
