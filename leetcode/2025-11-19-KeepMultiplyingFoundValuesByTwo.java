6648013c93a8497273dfa8c055f09c7905d0cd78
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-11-19 20:01:48 +0530

    Create 2025-11-19-KeepMultiplyingFoundValuesByTwo.java

---
+++ leetcode/2025-11-19-KeepMultiplyingFoundValuesByTwo.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasOriginal(int[] nums,int original)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==original) return true;
+        }
+        return false;
+    }
+    public int findFinalValue(int[] nums, int original) {
+        while(hasOriginal(nums,original))
+        {
+            original *= 2;
+        }
+        return original;
+    }
+}
