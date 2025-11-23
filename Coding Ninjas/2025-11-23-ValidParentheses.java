83c0ece983a82893daffd46bf4e660dd9bb7ad7f
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-11-23 21:32:36 +0530

    Create 2025-11-23-ValidParentheses.java

---
+++ Coding Ninjas/2025-11-23-ValidParentheses.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public static boolean isValidParenthesis(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) return false;
+
+                char top = st.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '['))
+                    return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
