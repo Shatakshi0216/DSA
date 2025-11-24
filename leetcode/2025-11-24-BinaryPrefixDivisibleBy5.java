050071144add68b3f1a36c7e1da92da78da98fca
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-11-24 12:33:20 +0530

    Create 2025-11-24-BinaryPrefixDivisibleBy5.java

---
+++ leetcode/2025-11-24-BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans = new ArrayList<>();
+        int num = 0;
+        for(int i=0;i<nums.length;i++)
+        {   
+            num = (num*2+nums[i])%5;
+            if(num==0) ans.add(true);
+            else ans.add(false);
+        }
+        return ans;
+    }
+}
