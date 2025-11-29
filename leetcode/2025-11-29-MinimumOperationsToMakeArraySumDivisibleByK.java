3c4386202122622c3e8ebc4ed0ab378001feac5e
Shatakshi <147087166+Shatakshi0216@users.noreply.github.com>
2025-11-29 20:24:51 +0530

    Create 2025-11-29-MinimumOperationsToMakeArraySumDivisibleByK.java

---
+++ leetcode/2025-11-29-MinimumOperationsToMakeArraySumDivisibleByK.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for(int i=0;i<nums.length;i++) sum+=nums[i];
+        if(sum<k) return sum;
+        int rem = sum%k;
+        if(rem==0) return 0;
+        return sum%k;
+
+    }
+}
